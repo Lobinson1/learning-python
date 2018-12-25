a7053bf7f17c13f407909ea0bd9b5dc07ca65792
chenjunxu <753720325@qq.com>
2018-12-25 17:27:01 +0800

    尝试获取新浪网页代码

---
+++ testMain.py
@@ -0,0 +1,14 @@
+from urllib import parse, request
+
+print('begin to get sina.html')
+# 构建请求头信息
+header_dict = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv:11.0) like Gecko'}
+url = 'https://www.sina.com.cn'
+# 构建请求
+req = request.Request(url=url, headers=header_dict)
+res = request.urlopen(req)
+res = res.read()
+f = open('D:/sina.html', 'wb')
+f.write(res)
+f.close()
+print('success!')
